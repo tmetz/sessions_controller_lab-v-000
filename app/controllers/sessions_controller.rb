c39d0711b40dc1eb8a62126e46b43b5d51bab729
Tammy Metz <tammy@tammymetz.com>
2019-03-20 21:09:03 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,13 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    session[:username] = params[:username]
+    redirect_to '/'
+  end
+
+  def destroy
+    session.delete :username
+  end
 end
