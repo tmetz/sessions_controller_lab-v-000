5ea0ea781bb74f6b1d14597928f649f799fc3f07
Tammy Metz <tammy@tammymetz.com>
2019-03-20 21:18:31 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if (!params[:name] || params[:name].empty?)
+      redirect_to controller: 'sessions', action: 'new'
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+  end
+end
